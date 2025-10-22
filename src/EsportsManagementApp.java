2c0f4a64eb42bc780b552c885c7e7401cdbebd1d
Deniz1923 <ozgurdeniz2018@gmail.com>
2025-10-22 12:50:11 +0300

    commit

--- src/EsportsManagementApp.java
+++ src/EsportsManagementApp.java
@@ -1,4 +1,17 @@
 public class EsportsManagementApp {
+
     String GAMES_FILE_PATH = "games.csv";
     String GAMERS_FILE_PATH = "gamers.csv";
+
+    public static void main(String[] args) {
+
+
+        Query query = new Query(matchManagement.getMatches(), pointsBoard);
+        query.queryHighestScoringMatch();
+        query.queryLowestScoringMatchAndContributor();
+        query.queryLowestBonusMatch();
+        query.queryHighestScoringGamer();
+        query.queryTotalTournamentPoints();
+        query.queryMedalDistribution();
+    }
 }
